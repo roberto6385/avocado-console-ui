d8cda9fb69b5e14ef8cbdc817ccf5b6ce2146b1f
seob717 <seob717@seob717ui-MacBookPro.local>
2021-03-29 18:47:13 +0900

    sftp websocket hooks

--- src/ws/sftp_ws.js
+++ src/ws/sftp_ws.js
@@ -230,119 +230,119 @@ const sftp_ws = ({
 			ws.send(msgObj.serializeBinary());
 		}
 
-		ws.binaryType = 'arraybuffer';
-		ws.onmessage = (evt) => {
-			if (evt.data instanceof ArrayBuffer) {
-				const message = SFTP.Message.deserializeBinary(evt.data);
-
-				if (message.getType() === SFTP.Message.Types.RESPONSE) {
-					const response = SFTP.Response.deserializeBinary(
-						message.getBody(),
-					);
-					console.log('[receive]response type', response.getType());
-					if (response.getType() === SFTP.Response.Types.CONNECT) {
-						const msgObj = SFTP.ConnectResponse.deserializeBinary(
-							response.getBody(),
-						);
-
-						if (msgObj.getStatus() === 'connected') {
-							resolve(msgObj.toObject());
-						}
-					} else if (
-						response.getType() === SFTP.Response.Types.DISCONNECT
-					) {
-						const msgObj = SFTP.DisconnectResponse.deserializeBinary(
-							response.getBody(),
-						);
-
-						if (msgObj.getStatus() === 'disconnected') {
-							resolve(msgObj.toObject());
-						}
-					} else if (
-						response.getType() === SFTP.Response.Types.MESSAGE
-					) {
-						const msgObj = SFTP.MessageResponse.deserializeBinary(
-							response.getBody(),
-						);
-
-						var percent = progress;
-
-						if (
-							msgObj.getStatus() !== undefined &&
-							msgObj.getStatus() === 'progress'
-						) {
-							percent = msgObj
-								.getResult()
-								.replace('percent : ', '');
-							console.log('[progress]..........', percent);
-						} else {
-							if (
-								keyword !== 'CommandByGet' &&
-								// keyword !== 'CommandByPut' &&
-								keyword !== 'EDIT'
-							) {
-								console.log(msgObj.toObject());
-								console.log(msgObj.getStatus());
-								resolve(msgObj.toObject());
-							}
-						}
-					} else if (
-						response.getType() === SFTP.Response.Types.FILE
-					) {
-						const msgObj = SFTP.FileResponse.deserializeBinary(
-							response.getBody(),
-						);
-
-						var arr = msgObj.getContents_asU8();
-
-						console.log('file as u8', arr);
-
-						fileBuffer = appendBuffer(fileBuffer, arr);
-						console.log('fileBuffer---> ', fileBuffer);
-
-						// 프로그래스바
-						var sum = getReceiveSum + arr.length;
-						const percent = (sum * 100) / msgObj.getFilesize();
-
-						console.log({
-							getReceiveSum: sum,
-							progress: percent,
-						});
-
-						if (msgObj.getLast() === true) {
-							const blob = new Blob([fileBuffer]);
-
-							// eslint-disable-next-line no-undef
-							fileBuffer = new ArrayBuffer(0);
-
-							if (keyword === 'EDIT') {
-								// let text = '';
-								blob.stream()
-									.getReader()
-									.read()
-									.then(({value, done}) => {
-										resolve(
-											new TextDecoder('utf-8').decode(
-												value,
-											),
-										);
-									});
-							} else {
-								var url = URL.createObjectURL(blob);
-								var a = document.createElement('a');
-								document.body.appendChild(a);
-								a.style = 'display: none';
-								a.href = url;
-								a.download = fileName;
-								a.click();
-								window.URL.revokeObjectURL(url);
-								resolve(msgObj.toObject());
-							}
-						}
-					}
-				}
-			}
-		};
+		// ws.binaryType = 'arraybuffer';
+		// ws.onmessage = (evt) => {
+		// 	if (evt.data instanceof ArrayBuffer) {
+		// 		const message = SFTP.Message.deserializeBinary(evt.data);
+		//
+		// 		if (message.getType() === SFTP.Message.Types.RESPONSE) {
+		// 			const response = SFTP.Response.deserializeBinary(
+		// 				message.getBody(),
+		// 			);
+		// 			console.log('[receive]response type', response.getType());
+		// 			if (response.getType() === SFTP.Response.Types.CONNECT) {
+		// 				const msgObj = SFTP.ConnectResponse.deserializeBinary(
+		// 					response.getBody(),
+		// 				);
+		//
+		// 				if (msgObj.getStatus() === 'connected') {
+		// 					resolve(msgObj.toObject());
+		// 				}
+		// 			} else if (
+		// 				response.getType() === SFTP.Response.Types.DISCONNECT
+		// 			) {
+		// 				const msgObj = SFTP.DisconnectResponse.deserializeBinary(
+		// 					response.getBody(),
+		// 				);
+		//
+		// 				if (msgObj.getStatus() === 'disconnected') {
+		// 					resolve(msgObj.toObject());
+		// 				}
+		// 			} else if (
+		// 				response.getType() === SFTP.Response.Types.MESSAGE
+		// 			) {
+		// 				const msgObj = SFTP.MessageResponse.deserializeBinary(
+		// 					response.getBody(),
+		// 				);
+		//
+		// 				var percent = progress;
+		//
+		// 				if (
+		// 					msgObj.getStatus() !== undefined &&
+		// 					msgObj.getStatus() === 'progress'
+		// 				) {
+		// 					percent = msgObj
+		// 						.getResult()
+		// 						.replace('percent : ', '');
+		// 					console.log('[progress]..........', percent);
+		// 				} else {
+		// 					if (
+		// 						keyword !== 'CommandByGet' &&
+		// 						// keyword !== 'CommandByPut' &&
+		// 						keyword !== 'EDIT'
+		// 					) {
+		// 						console.log(msgObj.toObject());
+		// 						console.log(msgObj.getStatus());
+		// 						resolve(msgObj.toObject());
+		// 					}
+		// 				}
+		// 			} else if (
+		// 				response.getType() === SFTP.Response.Types.FILE
+		// 			) {
+		// 				const msgObj = SFTP.FileResponse.deserializeBinary(
+		// 					response.getBody(),
+		// 				);
+		//
+		// 				var arr = msgObj.getContents_asU8();
+		//
+		// 				console.log('file as u8', arr);
+		//
+		// 				fileBuffer = appendBuffer(fileBuffer, arr);
+		// 				console.log('fileBuffer---> ', fileBuffer);
+		//
+		// 				// 프로그래스바
+		// 				var sum = getReceiveSum + arr.length;
+		// 				const percent = (sum * 100) / msgObj.getFilesize();
+		//
+		// 				console.log({
+		// 					getReceiveSum: sum,
+		// 					progress: percent,
+		// 				});
+		//
+		// 				if (msgObj.getLast() === true) {
+		// 					const blob = new Blob([fileBuffer]);
+		//
+		// 					// eslint-disable-next-line no-undef
+		// 					fileBuffer = new ArrayBuffer(0);
+		//
+		// 					if (keyword === 'EDIT') {
+		// 						// let text = '';
+		// 						blob.stream()
+		// 							.getReader()
+		// 							.read()
+		// 							.then(({value, done}) => {
+		// 								resolve(
+		// 									new TextDecoder('utf-8').decode(
+		// 										value,
+		// 									),
+		// 								);
+		// 							});
+		// 					} else {
+		// 						var url = URL.createObjectURL(blob);
+		// 						var a = document.createElement('a');
+		// 						document.body.appendChild(a);
+		// 						a.style = 'display: none';
+		// 						a.href = url;
+		// 						a.download = fileName;
+		// 						a.click();
+		// 						window.URL.revokeObjectURL(url);
+		// 						resolve(msgObj.toObject());
+		// 					}
+		// 				}
+		// 			}
+		// 		}
+		// 	}
+		// };
 	});
 };
 
