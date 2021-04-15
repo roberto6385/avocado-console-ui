453ed8d42144c80ee10d024bfc9b156708ce66c0
seob717 <seob717@seob717ui-MacBookPro.local>
2021-04-15 18:15:54 +0900

    remove task

--- src/sagas/sftp/index.js
+++ src/sagas/sftp/index.js
@@ -11,13 +11,14 @@ import {
 import {
 	CONNECTION_REQUEST,
 	CONNECTION_SUCCESS,
-	DISCONNECTION_REQUEST,
 	DISCONNECTION_SUCCESS,
 	errorAction,
 } from '../../reducers/sftp/index';
 import SFTP from '../../dist/sftp_pb';
-import {CLOSE_TAB, OPEN_TAB} from '../../reducers/common';
+import {OPEN_TAB} from '../../reducers/common';
 import sftp_ws from '../../ws/sftp_ws';
+
+import disconnectSaga from './sendDisconnect';
 
 function createWebsocket(payload) {
 	const server = payload.server;
@@ -105,56 +106,14 @@ function* sendConnect({payload}) {
 	}
 }
 
-function* sendDisconnect({payload}) {
-	console.log(payload);
-	const {socket, channel, id} = payload;
-	try {
-		yield call(sftp_ws, {
-			keyword: 'Disconnection',
-			ws: socket,
-		});
-		const data = yield take(channel);
-		if (data instanceof ArrayBuffer) {
-			const message = SFTP.Message.deserializeBinary(data);
-			if (message.getTypeCase() === SFTP.Message.TypeCase.RESPONSE) {
-				const response = message.getResponse();
-				console.log('response status: ', response.getStatus());
-				if (
-					response.getResponseCase() ===
-					SFTP.Response.ResponseCase.DISCONNECT
-				) {
-					const disconnect = response.getDisconnect();
-					console.log(disconnect);
-					yield put({
-						type: DISCONNECTION_SUCCESS,
-					});
-				}
-			}
-		}
-		yield take(
-			DISCONNECTION_SUCCESS,
-			yield put({
-				type: CLOSE_TAB,
-				data: id,
-			}),
-		);
-	} catch (err) {
-		yield put(errorAction('Error while disconnecting to the WebSocket'));
-	}
-}
-
 function* watchSendConnect() {
 	yield takeLatest(CONNECTION_REQUEST, sendConnect);
 }
 
-function* watchSendDisconnect() {
-	yield takeLatest(DISCONNECTION_REQUEST, sendDisconnect);
-}
-
 export default function* sftpSaga() {
-	const socketTask = yield all([
-		yield fork(watchSendConnect),
-		yield call(watchSendDisconnect),
-	]);
-	yield take(DISCONNECTION_SUCCESS, yield cancel(socketTask));
+	yield all([yield fork(watchSendConnect), yield fork(disconnectSaga)]);
+	yield take(
+		DISCONNECTION_SUCCESS,
+		yield cancel(yield fork(watchSendConnect)),
+	);
 }
---
+++ src/sagas/sftp/sendDisconnect.js
@@ -0,0 +1,55 @@
+import {all, call, fork, put, take, takeLatest} from 'redux-saga/effects';
+import {
+	DISCONNECTION_REQUEST,
+	DISCONNECTION_SUCCESS,
+	errorAction,
+} from '../../reducers/sftp/index';
+import sftp_ws from '../../ws/sftp_ws';
+import SFTP from '../../dist/sftp_pb';
+import {CLOSE_TAB} from '../../reducers/common';
+
+function* sendDisconnect({payload}) {
+	console.log(payload);
+	const {socket, channel, id} = payload;
+	try {
+		yield call(sftp_ws, {
+			keyword: 'Disconnection',
+			ws: socket,
+		});
+		const data = yield take(channel);
+		if (data instanceof ArrayBuffer) {
+			const message = SFTP.Message.deserializeBinary(data);
+			if (message.getTypeCase() === SFTP.Message.TypeCase.RESPONSE) {
+				const response = message.getResponse();
+				console.log('response status: ', response.getStatus());
+				if (
+					response.getResponseCase() ===
+					SFTP.Response.ResponseCase.DISCONNECT
+				) {
+					const disconnect = response.getDisconnect();
+					console.log(disconnect);
+					yield put({
+						type: DISCONNECTION_SUCCESS,
+					});
+				}
+			}
+		}
+		yield take(
+			DISCONNECTION_SUCCESS,
+			yield put({
+				type: CLOSE_TAB,
+				data: id,
+			}),
+		);
+	} catch (err) {
+		yield put(errorAction('Error while disconnecting to the WebSocket'));
+	}
+}
+
+function* watchSendDisconnect() {
+	yield takeLatest(DISCONNECTION_REQUEST, sendDisconnect);
+}
+
+export default function* disconnectSaga() {
+	yield all([fork(watchSendDisconnect)]);
+}
