a11b1c27c8eba012247a10821332b591235b2c60
seob717 <seob717@seob717ui-MacBookPro.local>
2021-05-13 18:20:06 +0900

    global style setting

---
+++ src/styles/global_design.js
@@ -0,0 +1,31 @@
+// color
+export const AVOCADO_COLOR = '#178082'; // logo, active icon color
+export const AVOCADO_HOVER_COLOR = '#e4f3f4'; // folder,server highlight color
+export const LIGHT_BACK_COLOR = '#f0f3f6'; // terminal, input background color
+export const ICON_DARK_COLOR = 'rgba(0,0,0,0.54)'; // file list nav icon color
+export const ICON_LIGHT_COLOR = 'rgba(60,76,81,0.54)'; // tab icon color
+export const FONT_COLOR = 'rgba(0,0,0,0.87)'; //other icon color
+export const FOOTER_BACK_COLOR = '#dee1e6';
+export const SFTP_DIRECTORY_COLOR = '#4ca6a8';
+export const POPUP_SIDE_COLOR = '#f8f9fa';
+export const CANCEL_BUTTON_COLOR = 'rgba(60,76,81,0.24)';
+export const LIGHT_MODE_BACK_COLOR = '#ffffff';
+
+// height
+export const AVOCADO_HEIGHT = '60px'; // tab container, logo container
+export const AVOCADO_SUB_HEIGHT = '50px'; // new folder container, (ssht, sftp nav)
+export const AVOCADO_FOLDER_HEIGHT = '40px'; // folder, server height
+export const FOOTER_HEIGHT = '26px'; // footer height
+export const SFTP_TABLE_HEIGHT = '48px';
+export const BUTTON_HEIGHT = '20px';
+
+// width
+export const LEFT_SIDE_WIDTH = '256px'; // left side main nav width
+export const RIGHT_SIDE_WIDTH = '300px';
+
+// font-size
+export const AVOCADO_FONTSIZE = '14px';
+export const AVOCADO_LOGO_FONTSIZE = '24px';
+
+// font family
+export const ROBOTO_FONT = 'Roboto';
