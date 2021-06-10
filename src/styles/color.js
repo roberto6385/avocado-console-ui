4f57ab19423c9d380f2aa1a39d392c2ac41a6d5e
seob717 <seob717@seob717ui-MacBookPro.local>
2021-06-10 15:43:48 +0900

    style - color

---
+++ src/styles/color.js
@@ -0,0 +1,32 @@
+// light button color
+export const L_HIGHLIGHT = '#e4f3f4';
+export const L_GREEN_HOVER = '#389193';
+export const L_GREEN_NORMAL = '#178082';
+export const L_GREEN_ACTIVE = '#0a6f71';
+export const L_RED_HOVER = '#d45959';
+export const L_RED_NORMAL = '#de6565';
+export const L_RED_ACTIVE = '#b84646';
+
+// dark button color
+export const D_HIGHLIGHT = 'rgba(105,211,205,0.05)';
+export const D_GREEN_HOVER = '#69d3cd';
+export const D_GREEN_NORMAL = '#44c8c0';
+export const D_GREEN_ACTIVE = '#31bbb3';
+export const D_RED_HOVER = '#e27777';
+export const D_RED_NORMAL = '#de6565';
+export const D_RED_ACTIVE = '#d45959';
+
+// dark background color
+export const D_BACK = '#0b141c';
+export const D_BACK_APPBAR = '#121d28';
+export const D_BACK_TOOLBAR_INPUT = '#21303e';
+export const D_BACK_DROPDOWN = '#253545';
+export const D_BACK1 = '#17232e';
+export const D_BACK2 = '#1b2935';
+
+// indigo color
+export const I_BACK_NAV = '#1e364c';
+export const I_BACK_SETTING = '#1c3246';
+export const I_BACK_SELECT_MENU = '#1e364c';
+export const I_BACK_POPUP = '#223c51';
+export const I_BACK_DROP = '#325165';
