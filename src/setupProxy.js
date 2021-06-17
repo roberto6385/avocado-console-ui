5ad522ba7407ee45b96a4b9f29aba0f8f14e68d9
seob717 <seob717@seob717ui-MacBookPro.local>
2021-06-17 16:53:08 +0900

    pathRewrite

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,13 +2,16 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 exports.module = (app) => {
 	app.use(
-		createProxyMiddleware('/oauth2', {
+		createProxyMiddleware('/', {
 			target:
 				'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
 			changeOrigin: true,
 			onProxyRes: function (proxyRes) {
 				proxyRes.headers['Access-Control-Allow-Origin'] =
 					'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200';
+			},
+			pathRewrite: {
+				'^/': '', // 하위 url 초기화
 			},
 		}),
 	);
