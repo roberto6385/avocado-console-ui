00507f783a8281e5d661cf6f086864949aeba03b
seob717 <seob717@seob717ui-MacBookPro.local>
2021-06-17 13:24:22 +0900

    setup proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = (app) => {
 	app.use(
-		createProxyMiddleware('/oauth2', {
+		createProxyMiddleware('/api', {
 			// target: 'http://211.253.24.87:10072',
 			target:
 				'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
