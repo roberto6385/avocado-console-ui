ac9f0cc22cd4edc295313453bf0aa7cb1777d70a
seob717 <seob717@seob717ui-MacBookPro.local>
2021-06-17 18:00:26 +0900

    *

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -7,8 +7,7 @@ exports.module = (app) => {
 				'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
 			changeOrigin: true,
 			onProxyRes: function (proxyRes) {
-				proxyRes.headers['Access-Control-Allow-Origin'] =
-					'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200';
+				proxyRes.headers['Access-Control-Allow-Origin'] = '*';
 			},
 			pathRewrite: {
 				'^/': '', // 하위 url 초기화
