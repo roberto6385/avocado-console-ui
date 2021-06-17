0f3a32b59b1c6596141e9a225268a5f92ed43e41
seob717 <seob717@seob717ui-MacBookPro.local>
2021-06-17 15:21:25 +0900

    proxy 수정중...

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,6 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = (app) => {
 	app.use(
 		createProxyMiddleware('/', {
-			// target: 'http://211.253.24.87:10072',
 			target:
 				'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
 			changeOrigin: true,
