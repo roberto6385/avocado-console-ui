0046f627ae3014d0420ddb4c97f5ded8052d9ccd
seob717 <seob717@seob717ui-MacBookPro.local>
2021-06-17 16:49:28 +0900

    access-control-allow-origin

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,11 +6,9 @@ exports.module = (app) => {
 			target:
 				'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
 			changeOrigin: true,
-			onProxyReq: function (request) {
-				request.setHeader(
-					'url',
-					'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
-				);
+			onProxyRes: function (proxyRes) {
+				proxyRes.headers['Access-Control-Allow-Origin'] =
+					'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200';
 			},
 		}),
 	);
