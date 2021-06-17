b9a2d190edba7e7a1eff7268294152ab33de73cf
seob717 <seob717@seob717ui-MacBookPro.local>
2021-06-17 16:05:49 +0900

    express library

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,27 +1,28 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
+const express = require('express');
 
-module.exports = (app) => {
-	app.use(
-		createProxyMiddleware('/', {
-			target:
+const app = express();
+
+app.use(
+	createProxyMiddleware('/', {
+		target:
+			'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
+		changeOrigin: true,
+		onProxyReq: function (request) {
+			request.setHeader(
+				'origin',
 				'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
-			changeOrigin: true,
-			onProxyReq: function (request) {
-				request.setHeader(
-					'origin',
-					'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
-				);
-			},
-		}),
-	);
-	// app.use(
-	// 	createProxyMiddleware(['/', '/api', '/socket.io'], {
-	// 		target: 'http://nginx:80',
-	// 		changeOrigin: true,
-	// 		ws: true,
-	// 		router: {
-	// 			'/socket.io': 'ws://nginx:80',
-	// 		},
-	// 	}),
-	// );
-};
+			);
+		},
+	}),
+);
+// app.use(
+// 	createProxyMiddleware(['/', '/api', '/socket.io'], {
+// 		target: 'http://nginx:80',
+// 		changeOrigin: true,
+// 		ws: true,
+// 		router: {
+// 			'/socket.io': 'ws://nginx:80',
+// 		},
+// 	}),
+// );
