d56503219a436d4d785671b23bb750cd41508a8a
seob717 <seob717@seob717ui-MacBookPro.local>
2021-06-17 16:40:16 +0900

    / => /oauth2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 exports.module = (app) => {
 	app.use(
-		createProxyMiddleware('/', {
+		createProxyMiddleware('/oauth2', {
 			target:
 				'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
 			changeOrigin: true,
