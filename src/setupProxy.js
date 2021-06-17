27803503be2fd9e1696e82148a8889034e2c92ac
seob717 <seob717@seob717ui-MacBookPro.local>
2021-06-17 16:22:06 +0900

    remove express

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,28 +1,17 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
-const express = require('express');
 
-const app = express();
-
-app.use(
-	createProxyMiddleware('/', {
-		target:
-			'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
-		changeOrigin: true,
-		onProxyReq: function (request) {
-			request.setHeader(
-				'url',
+exports.module = (app) => {
+	app.use(
+		createProxyMiddleware('/', {
+			target:
 				'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
-			);
-		},
-	}),
-);
-// app.use(
-// 	createProxyMiddleware(['/', '/api', '/socket.io'], {
-// 		target: 'http://nginx:80',
-// 		changeOrigin: true,
-// 		ws: true,
-// 		router: {
-// 			'/socket.io': 'ws://nginx:80',
-// 		},
-// 	}),
-// );
+			changeOrigin: true,
+			onProxyReq: function (request) {
+				request.setHeader(
+					'url',
+					'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
+				);
+			},
+		}),
+	);
+};
