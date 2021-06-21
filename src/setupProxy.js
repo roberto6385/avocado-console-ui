355add463d41cca29f46ae954d6cdad130934399
seob717 <seob717@seob717ui-MacBookPro.local>
2021-06-21 11:33:47 +0900

    sftp dist file change

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = (app) => {
 	app.use(
-		createProxyMiddleware(' ddd/', {
+		createProxyMiddleware('/', {
 			target:
 				'http://ec2-3-36-116-0.ap-northeast-2.compute.amazonaws.com:10200',
 			changeOrigin: true,
