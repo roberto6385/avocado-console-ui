609c9d884f9cc028bc10a59a96ef8311cf03720e
ambacc244netand <ambacc244@netand.co.kr>
2021-06-24 22:16:21 +0900

    google login redirect

--- src/pages/Redirect.js
+++ src/pages/Redirect.js
@@ -63,11 +63,6 @@ const Redirect = () => {
 							},
 						},
 					)
-					.catch((error) => {
-						console.log(error);
-						console.log('HERER');
-						return <Redirect to={'/signin'} />;
-					})
 					// .then((response) => verify(response.data.access_token));
 					.then((google) => {
 						console.log(google.data);
@@ -112,6 +107,11 @@ const Redirect = () => {
 										localStorage.removeItem('password');
 									});
 							});
+					})
+					.catch((error) => {
+						console.log(error);
+						console.log('HERER');
+						return <Redirect to={'/signin'} />;
 					});
 			});
 	}, []);
