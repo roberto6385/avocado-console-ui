9583c4ec52efac19a0eac9468a4b816793056094
seob717 <seob717@seob717ui-MacBookPro.local>
2021-08-12 12:15:04 +0900

    persist session bug

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,18 +4,18 @@ import common from './common';
 import ssh from './ssh';
 import sftp from './sftp';
 import dialogBoxs from './dialogBoxs';
-import {USER_RESOURCE, userResourceReducer} from './api/userResource';
-import {AUTH, authReducer} from './api/auth';
+import {userResourceReducer} from './api/userResource';
+import {authReducer} from './api/auth';
 import storage from 'redux-persist/lib/storage';
 import storageSession from 'redux-persist/lib/storage/session';
 import {createWhitelistFilter} from 'redux-persist-transform-filter';
 
-export const authFilter = createWhitelistFilter(AUTH, [AUTH]);
+export const authFilter = createWhitelistFilter('auth', 'auth');
 
 const persistConfig = {
 	key: 'root',
 	storage: storageSession,
-	whitelist: [AUTH],
+	whitelist: 'auth',
 	transforms: [authFilter],
 };
 
@@ -36,8 +36,8 @@ const rootReducer = combineReducers({
 	sftp,
 	ssh: persistReducer(sshLocalPersistConfig, ssh),
 	dialogBoxs,
-	[AUTH]: authReducer,
-	[USER_RESOURCE]: userResourceReducer,
+	auth: authReducer,
+	userResource: userResourceReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
